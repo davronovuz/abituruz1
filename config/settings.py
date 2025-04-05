76c031d2d419f87dfe0d4e2da1013e0d1b389a0b
abdufattohfattoyev <abdufattohfattoyev0@gmail.com>
2025-04-05 10:39:06 +0500

    user modelsda userconfirmation va account tayyor

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'apps.utility',
+    'apps.account',
+    'apps.payment',
+    'apps.quizz',
+    # Third party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
