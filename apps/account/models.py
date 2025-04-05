b04aba168c64b9604a6052326c311ccd1e557077
davronovuz <davronovtatu@gmail.com>
2025-04-05 11:00:43 +0500

    ozgarishlar

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from apps.utility.models import Base
+from django.core.validators import FileExtensionValidator,RegexValidator
 
-# Create your models here.
+
+#CONSTANT FIELDS
+NEW,CODE_VERIFIED,DONE='new','code_verified','done'
+STUDENT,TEACHER,ADMIN,SUPERADMIN='student','teacher','admin','superadmin'
+EXPIRATION_TIME=2
+
+class User(Base,AbstractUser):
+    phone_regex=RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Telfon raqam +998901234567 formatda bo'lishi kerak")
+    full_name=models.CharField(max_length=255,null=False,blank=False)
+    phone=models.CharField(validators=[phone_regex],max_length=17,unique=True)
+    avatar=models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg','png','jpeg','webp','gif','jfif'])]
+        )
+    date_of_birth=models.DateField(null=True,blank=True)
+
+    USER_ROLES = (
+        (STUDENT, STUDENT),
+        (TEACHER, TEACHER),
+        (ADMIN, ADMIN),
+        (SUPERADMIN, SUPERADMIN),
+    )
+    user_role=models.CharField(max_length=20,choices=USER_ROLES,default=STUDENT)
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        db_table='users'
+        verbose_name='user'
+        verbose_name_plural='users'
+        ordering=['created_at']
+
+
+
+
+
+
+
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #LOCAL APPS
+    'apps.account',
+    'apps.payment',
+    'apps.quizz',
+    'apps.utility',
+
+    #THIRD PARTY APPS
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
